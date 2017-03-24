6d12bb6167689c981102d3f87d004de5ca21582a
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-03-24 20:58:54 +0200

    Return MVP data from emotions api

--- index.ios.js
+++ index.ios.js
@@ -6,11 +6,25 @@ import {
   View
 } from 'react-native';
 import Camera from 'react-native-camera';
+import RNFetchBlob from 'react-native-fetch-blob';
+
+import apiKey from './apiKey';
 
 export default class emotions extends Component {
   takePicture() {
     this.camera.capture()
-      .then(data => console.log(data))
+      .then(({data}) => {
+        RNFetchBlob.fetch('POST', 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize', {
+          'Ocp-Apim-Subscription-Key': apiKey,
+          'Content-Type': 'application/octet-stream',
+        }, data)
+        .then(response => {
+          console.log('response', JSON.parse(response.data));
+        })
+        .catch(error => {
+          console.log('error', error);
+        })
+      })
       .catch(err => console.error(err));
   }
 
@@ -21,7 +35,8 @@ export default class emotions extends Component {
           ref={camera => {this.camera = camera;}}
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill}
-          captureTarget={Camera.constants.CaptureTarget.temp}
+          captureTarget={Camera.constants.CaptureTarget.memory}
+          type={Camera.constants.Type.front}
         >
           <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
         </Camera>
